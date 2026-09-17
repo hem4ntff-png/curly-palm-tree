e7de68a917e2ce09b163941f60b0c2da8ff6a242
hem4ntff-png <hem4ntff@gmail.com>
2026-09-17 09:09:59 +0530

    Update valid API key in app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # ✅ Valid API keys
 VALID_API_KEYS = {
-    "saito"  # don't change warna api or bot dono nhi chalega 
+    "batmanxd"  # don't change warna api or bot dono nhi chalega 
 }
 
 # 🔢 Like limit tracking
